7bbec1788c7abced6d16de701a5ec8bbc8128191
hunterbugs <79522220+hunterbugs@users.noreply.github.com>
2023-10-28 20:33:20 +0800

    commit

--- js/task-10.js
+++ js/task-10.js
@@ -3,34 +3,3 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, "0")}`;
 }
-
-function createBoxes(amount) {
-  const boxesDiv = document.getElementById("boxes");
-  boxesDiv.innerHTML = "";
-
-  let boxSize = 30;
-  for (let i = 0; i < amount; i++) {
-    const box = document.createElement("div");
-    box.style.width = boxSize + "px";
-    box.style.height = boxSize + "px";
-    box.style.backgroundColor = getRandomHexColor();
-    boxesDiv.appendChild(box);
-
-    boxSize += 10;
-  }
-}
-
-function destroyBoxes() {
-  const boxesDiv = document.getElementById("boxes");
-  boxesDiv.innerHTML = "";
-}
-
-document.getElementById("createButton").addEventListener("click", function () {
-  const boxCountInput = document.getElementById("boxCountInput");
-  const amount = parseInt(boxCountInput.value, 10);
-  createBoxes(amount);
-});
-
-document
-  .getElementById("destroyButton")
-  .addEventListener("click", destroyBoxes);
